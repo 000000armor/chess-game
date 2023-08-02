94d6099eb46f6aef4692a91c419f1bbe12b4d1d6
David Sevikian <david.sevikian.work@gmail.com>
2023-08-02 17:49:35 +0400

    edit types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,12 +1,6 @@
 import { Knight } from '@/entities/pieces';
 import { FunctionComponent, SVGProps } from 'react';
 
-export type Side = 'white' | 'black';
-
-export type SvgElement = FunctionComponent<
-  SVGProps<SVGSVGElement> & { title?: string | undefined }
->;
-
 export const PiecesRecord = {
   rook: 'rook',
   knight: 'knight',
@@ -15,6 +9,17 @@ export const PiecesRecord = {
   king: 'king',
   pawn: 'pawn',
 } as const;
+
+export const SidesRecord = {
+  white: 'white',
+  black: 'black',
+} as const;
+
+export type Side = (typeof SidesRecord)[keyof typeof SidesRecord];
+
+export type SvgElement = FunctionComponent<
+  SVGProps<SVGSVGElement> & { title?: string | undefined }
+>;
 
 export type Piece = (typeof PiecesRecord)[keyof typeof PiecesRecord];
 
