9c2fab924132a97ec183d8ec85a2555da9a6e3f6
David Sevikian <david.sevikian.work@gmail.com>
2023-08-03 19:45:03 +0400

    edit squares layer

--- src/components/squares-layer/squares-layer.tsx
+++ src/components/squares-layer/squares-layer.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './squares-layer.styles.css';
 
 export const SquaresLayer = () => {
   const board: null[] = Array(64).fill(null);
 
   return (
-    <div className='board-layer'>
+    <div className='squares-layer'>
       {board.map((_, index) => (
-        <div className={`field `} key={index} />
+        <div className='square' key={index} />
       ))}
     </div>
   );
